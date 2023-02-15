36b9f798800fac69fae59e2cbb5872a0ba7bfe03
John Rey <tmq.john.t@gmail.com>
2023-02-15 22:00:52 +0800

    Server - Initialize Group Schema

---
+++ server/src/schema/GroupSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const GroupSchema = new mongoose.Model(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    tasks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Group", GroupSchema);
