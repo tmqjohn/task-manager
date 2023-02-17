faa12bf9ad0aa2cf8d4de6ff5ed17c9329a4fe5b
John Rey <tmq.john.t@gmail.com>
2023-02-18 04:12:55 +0800

    Client - Add role access for manage project button

--- client/src/components/routes/project/Project.jsx
+++ client/src/components/routes/project/Project.jsx
@@ -3,14 +3,16 @@ import { useParams } from "react-router-dom";
 
 import ManageProject from "./ManageProject";
 
-import { useProjectStore } from "../../../store/store";
+import { useProjectStore, useUserStore } from "../../../store/store";
 
 const Projects = () => {
   const projects = useProjectStore((state) => state.projects);
+  const userDetails = useUserStore((state) => state.userDetails);
   const [selectedProject, setSelectedProject] = useState([]);
   const [projectDefaults, setProjectDefaults] = useState(false);
 
   let { projectId } = useParams();
+  let manageBtn;
 
   useEffect(() => {
     setSelectedProject(
@@ -22,19 +24,25 @@ const Projects = () => {
     setProjectDefaults((prev) => !prev);
   }
 
+  if (selectedProject[0]) {
+    manageBtn = selectedProject[0].owner.includes(userDetails?._id) ? (
+      <button
+        className="btn ms-auto"
+        data-bs-toggle="modal"
+        data-bs-target="#manageProjectPrompt"
+        onClick={handleDefaultInput}
+      >
+        <img src="/project_settings.svg" /> Manage Project
+      </button>
+    ) : null;
+  }
+
   return (
     <>
       <div className="project-title ps-2 pt-2">
         <div className="d-flex">
           <h2 className="">{selectedProject[0]?.title}</h2>
-          <button
-            className="btn ms-auto"
-            data-bs-toggle="modal"
-            data-bs-target="#manageProjectPrompt"
-            onClick={handleDefaultInput}
-          >
-            <img src="/project_settings.svg" /> Manage Project
-          </button>
+          {manageBtn}
         </div>
 
         <p className="text-secondary fs-6">{selectedProject[0]?.desc}</p>
