5bbb320fe0b2c4e969b76199c1b341d7fc8356e2
John Rey <tmq.john.t@gmail.com>
2023-02-22 06:25:24 +0800

    Client - Add groups api

---
+++ client/src/components/api/group.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
+
+/**
+ * @POST request
+ * http://serverurl/api/group
+ */
+export async function addNewGroup(projectId) {
+  try {
+    const groups = await axios.post("/api/group", {
+      title: "New Group",
+    });
+
+    await axios.patch("/api/project/groups/add", {
+      groups: groups.data,
+      projectId,
+    });
+
+    return true;
+  } catch (error) {
+    toast.dismiss();
+    toast.error(error.response ? error.response.data.message : error.message);
+  }
+}
+
+/**
+ * @PUT request
+ * http://serverurl/api/group/:groupId
+ */
+export async function updateGroup(title, groupId) {
+  try {
+    await axios.put(`api/group/${groupId}`, {
+      title,
+    });
+
+    return true;
+  } catch (error) {
+    toast.dismiss();
+    toast.error(error.response ? error.response.data.message : error.message);
+  }
+}
+
+/**
+ * @DELETE request
+ * http://serverurl/api/group/:groupId
+ */
+export async function deleteGroup(groupId, projectId) {
+  try {
+    await axios.delete(`api/group/${groupId}`);
+
+    const response = await axios.patch(
+      `/api/project/groups/delete/${projectId}`,
+      {
+        groupId,
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    toast.dismiss();
+    toast.error(error.response ? error.response.data.message : error.message);
+  }
+}
