cd1908a85cb1880ca8b6b991badfbc86e4dfba15
John Rey <tmq.john.t@gmail.com>
2023-02-22 18:13:05 +0800

    Client - Add modal prompts for groups, profile and confirm components

---
+++ client/src/components/layout/ModalLayout/GroupsModal.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const GroupsModal = ({
+  id,
+  title,
+  inputId,
+  inputTitleRef,
+  closeBtnRef,
+  submitFunction,
+  submitBtnLabel,
+}) => {
+  return (
+    <div
+      className="modal fade"
+      id={id}
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabIndex="-1"
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5">{title}</h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+            ></button>
+          </div>
+          <form autoComplete="false" onSubmit={(e) => e.preventDefault()}>
+            <div className="modal-body">
+              <label htmlFor={inputTitleRef} className="col-form-label">
+                Title:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id={inputId}
+                ref={inputTitleRef}
+                required
+              />
+            </div>
+
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+                ref={closeBtnRef}
+              >
+                Back
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={submitFunction}
+              >
+                {submitBtnLabel}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GroupsModal;
---
+++ client/src/components/layout/ModalLayout/ProfileModal.jsx
@@ -0,0 +1,120 @@
+import React, { useRef, useEffect } from "react";
+
+const ProfileModal = ({
+  id,
+  title,
+  inputId,
+  updateProfile,
+  defaultProfileInputs,
+  userDetails,
+}) => {
+  const usernameInput = useRef();
+  const passwordInput = useRef();
+  const fullNameInput = useRef();
+  const emailInput = useRef();
+
+  useEffect(() => {
+    usernameInput.current.value = userDetails.username;
+    passwordInput.current.value = "";
+    fullNameInput.current.value = userDetails.fullName;
+    emailInput.current.value = userDetails.email;
+  }, [defaultProfileInputs]);
+
+  return (
+    <div
+      className="modal fade"
+      id={id}
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabIndex="-1"
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5">{title}</h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+            ></button>
+          </div>
+          <form autoComplete="off">
+            <div className="modal-body">
+              <section className="mb-3">
+                <label htmlFor={inputId.username} className="col-form-label">
+                  Username:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id={inputId.username}
+                  ref={usernameInput}
+                  disabled
+                />
+              </section>
+              <section className="mb-3">
+                <label htmlFor={inputId.password} className="col-form-label">
+                  Password:
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id={inputId.password}
+                  ref={passwordInput}
+                />
+              </section>
+              <section className="mb-3">
+                <label htmlFor={inputId.fullName} className="col-form-label">
+                  Full Name:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id={inputId.fullName}
+                  ref={fullNameInput}
+                  required
+                />
+              </section>
+              <section className="mb-3">
+                <label htmlFor={inputId.email} className="col-form-label">
+                  Email:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id={inputId.email}
+                  ref={emailInput}
+                  required
+                />
+              </section>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={async () =>
+                  await updateProfile(
+                    passwordInput.current.value,
+                    fullNameInput.current.value,
+                    emailInput.current.value
+                  )
+                }
+              >
+                Update
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileModal;
