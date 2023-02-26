0536f836041719c8ca26e71a332a62065b4f3194
John Rey <tmq.john.t@gmail.com>
2023-02-27 06:34:21 +0800

    Server - Add controller operations for task

--- server/src/controllers/taskControllers.js
+++ server/src/controllers/taskControllers.js
@@ -6,7 +6,13 @@ const Task = require("../schema/TaskSchema");
  * /api/task
  */
 const getAllTasks = asyncHandler(async (req, res) => {
-  res.send("@GET task");
+  const foundTasks = await Task.find().lean();
+
+  if (foundTasks) {
+    res.status(200).json(foundTasks);
+  } else {
+    res.status(500).json({ message: "No Tasks Found" });
+  }
 });
 
 /**@ POST request
@@ -14,23 +20,83 @@ const getAllTasks = asyncHandler(async (req, res) => {
  * /api/task
  */
 const addNewTask = asyncHandler(async (req, res) => {
-  res.send("@POST task");
+  const { title, dueDate, active, note } = req.body;
+
+  const newTask = new Task({
+    title,
+    dueDate,
+    active,
+    note,
+  });
+
+  const result = await newTask.save();
+
+  if (result) {
+    res.status(200).json(newTask);
+  } else {
+    res.status(500).json({ message: "There was an error creating a new task" });
+  }
 });
 
 /**@ PUT request
  * updates a task
- * /api/task
+ * /api/task/:taskId
  */
 const updateTask = asyncHandler(async (req, res) => {
-  res.send("@PUT task");
+  const { title, dueDate, active, note } = req.body;
+  const { taskId } = req.params;
+
+  const foundTask = await Task.findById(taskId).exec();
+
+  foundTask.title = title;
+  foundTask.dueDate = dueDate;
+  foundTask.active = active;
+  foundTask.note = note;
+
+  const result = await foundTask.save();
+
+  if (result === foundTask) {
+    res.status(200).json(foundTask);
+  } else {
+    res.status(500).json({ message: "There was an error updating task" });
+  }
 });
 
 /**@ DELETE request
  * deletes a task
- * /api/task
+ * /api/task/:taskId
  */
 const deleteTask = asyncHandler(async (req, res) => {
-  res.send("@DELETE task");
+  const { taskId } = req.params;
+
+  const result = await Task.findByIdAndDelete(taskId).exec();
+
+  console.log(result);
+
+  if (result) {
+    res.status(200).json({ message: "Task has been deleted successfully" });
+  } else {
+    res.status(500).json({ message: "There was an error deleting task" });
+  }
+});
+
+/**@ PUT request
+ * deletes tasks from a deleted group
+ * /api/task/group
+ */
+const deleteAllTask = asyncHandler(async (req, res) => {
+  const { taskIds } = req.body;
+
+  const result = await Group.deleteMany({ _id: taskIds });
+
+  if (result.acknowledged) {
+    res.status(200).json({
+      message:
+        "Tasks associated with the deleted group has been deleted successfully",
+    });
+  } else {
+    res.status(400).json({ message: "There was an error deleting the groups" });
+  }
 });
 
 module.exports = {
@@ -38,4 +104,5 @@ module.exports = {
   addNewTask,
   updateTask,
   deleteTask,
+  deleteAllTask,
 };
