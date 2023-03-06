59738fdd8753e47b711421a7cc08fbe6e4d2236e
John Rey <tmq.john.t@gmail.com>
2023-03-06 10:18:57 +0800

    Client - Initiated chat function with socket

--- client/src/components/routes/project/Chat.jsx
+++ client/src/components/routes/project/Chat.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-import { useProjectStore } from "../../../store/store";
+import {
+  useProjectStore,
+  useUserStore,
+  useChatStore,
+} from "../../../store/store";
+
+import { sendMessage, receiveMessage } from "../../../helpers/socket";
 
 import "./styles/project.css";
 
+// TODO: declare at MainInit socket and put socket in store
+
 const Chat = () => {
+  const selectedProject = useProjectStore((state) => state.selectedProject);
+  const userDetails = useUserStore((state) => state.userDetails);
+  const socket = useChatStore((state) => state.socket);
+
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  let { projectId } = useParams();
+
   const sampleChatBubble = [
-    ["Hi!aaaaaa", true, "Janella"],
-    ["Hello!aaaaaaaaaa", false, "John Rey"],
-    ["Hello!aaaaaaaaaa", true, "Aishia"],
-    ["Hello!aaaaaaaaaa", true, "Janella"],
-    ["Hello!aaaaaaaaaa", false, "John Rey"],
+    ["Hi!aaaaaa", true, "Janella Reine Tungul"],
+    ["Hello!aaaaaaaaaaaaaaaaaaaa", false, "John Rey Tungul"],
+    ["Hello!aaaaaaaaaa", true, "Aishia Echague"],
+    ["Hello!aaaaaaaaaa", true, "Janella Tungul"],
+    ["Hello!aaaaaaaaaa", false, "John Rey Tungul"],
   ];
 
-  const selectedProject = useProjectStore((state) => state.selectedProject);
+  function handleSendMessage() {
+    sendMessage(
+      {
+        name: userDetails.fullName,
+        room: projectId,
+        message,
+      },
+      socket
+    );
+  }
+
+  useEffect(() => {
+    receiveMessage(socket, (message) => {
+      setMessageReceived(message);
+    });
+  }, [socket]);
 
   return (
     <>
@@ -46,15 +79,21 @@ const Chat = () => {
         </section>
         <section className="offcanvas-footer d-flex flex-row border border-top p-3">
           <div className="input-send-divider w-100 h-100 me-2">
-            <form autoComplete="off">
-              <textarea className="chat-input form-control" />
+            <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
+              <textarea
+                className="chat-input form-control"
+                onChange={(event) => setMessage(event.target.value)}
+              />
             </form>
             <button className="btn border border-0 p-0 pe-1 mt-1">
               <img src="/attach_small.svg" /> Attach Files
             </button>
           </div>
           <div className="send-button">
-            <button className="btn btn-primary border border-0 p-1">
+            <button
+              className="btn btn-primary border border-0 p-1"
+              onClick={() => handleSendMessage()}
+            >
               <img src="/send_big.svg" />
             </button>
           </div>
