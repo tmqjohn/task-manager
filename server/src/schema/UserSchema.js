d4c0e34640e1602c9d8001b6534bd9da8ad8ca97
John Rey <tmq.john.t@gmail.com>
2023-03-12 13:14:50 +0800

    Server - Add googleId field for user schema

--- server/src/schema/UserSchema.js
+++ server/src/schema/UserSchema.js
@@ -16,6 +16,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       unique: true,
     },
+    googleId: {
+      type: String,
+      unique: true,
+    },
   },
   {
     timestamps: true,
