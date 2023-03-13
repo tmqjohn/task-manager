2805ad007ee13fe9dad29e56bca42aa58df6957c
John Rey <tmq.john.t@gmail.com>
2023-03-13 20:29:19 +0800

    Client - Integrated google singup and login in front end

--- client/src/components/routes/login/GoogleLogin.jsx
+++ client/src/components/routes/login/GoogleLogin.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useGoogleLogin } from "@react-oauth/google";
 
-const GoogleLogin = ({ formText }) => {
+import { googleRegister, googleLogin } from "../../api/user";
+
+const GoogleLogin = ({ method }) => {
+  const navigate = useNavigate();
+
+  const handleGoogleAccess = useGoogleLogin({
+    onSuccess: async (googleAccessToken) => {
+      if (method === "sign-up") {
+        const { googleRegisterToken } = await googleRegister(googleAccessToken);
+
+        if (googleRegisterToken) {
+          const isSuccess = await googleLogin(googleRegisterToken);
+
+          if (isSuccess) navigate("/");
+        }
+      }
+
+      if (method === "login") {
+        const isSuccess = await googleLogin(googleAccessToken);
+
+        if (isSuccess) navigate("/");
+      }
+    },
+  });
+
   return (
     <>
       <hr className="my-1" />
-      <p className="mb-4 text-center">Or {formText} with</p>
-      <button className="google btn btn-outline-dark position-relative translate-middle">
+      <p className="mb-4 text-center">Or {method} with</p>
+      <button
+        className="google btn btn-outline-dark position-relative translate-middle"
+        onClick={() => handleGoogleAccess()}
+      >
         <img
           className="me-2"
           width="20"
