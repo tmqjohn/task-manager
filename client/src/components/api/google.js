67cf353272ce67429245252fa9509cf30dc3698e
John Rey <tmq.john.t@gmail.com>
2023-03-21 02:18:58 +0800

    Client - Add google api

---
+++ client/src/components/api/google.js
@@ -0,0 +1,30 @@
+const SCOPES = [
+  "https://www.googleapis.com/auth/drive",
+  "https://www.googleapis.com/auth/drive.file",
+];
+
+const DISCOVERY_DOCS = [
+  "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+];
+
+export async function gapiPickerLoad() {
+  window.gapi.load("picker", initGapiPickerLoad);
+}
+
+export function initGapiPickerLoad() {
+  google.accounts.oauth2.initTokenClient({
+    client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    scope: SCOPES,
+  });
+}
+
+export async function gapiDriveLoad() {
+  await window.gapi.load("client", initGapiDriveClient);
+}
+
+async function initGapiDriveClient() {
+  await window.gapi.client.init({
+    apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
+    discoveryDocs: DISCOVERY_DOCS,
+  });
+}
