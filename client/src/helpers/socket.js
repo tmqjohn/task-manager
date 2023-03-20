55e2d9dad8bdc334d245b5575484755d41b2fcea
John Rey <tmq.john.t@gmail.com>
2023-03-21 06:58:54 +0800

    Client - Add emit and on event when project changes and update in socket helpers

--- client/src/helpers/socket.js
+++ client/src/helpers/socket.js
@@ -1,9 +1,19 @@
+//emit events
+export function projectChanges(socket) {
+  socket.emit("project-change");
+}
+
 export function joinProjectChat(data, socket) {
   socket.emit("join-room", data);
 }
 
 export function sendMessage(data, socket) {
   socket.emit("send-message", data);
+}
+
+// on events
+export function updateProject(socket, updateProject) {
+  socket.on("project-update", () => updateProject());
 }
 
 export function receiveMessage(socket, getMessage) {
