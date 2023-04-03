d8dd36edcf31e925e60a19ff391250c0c49eeb7d
John Rey <tmq.john.t@gmail.com>
2023-04-04 01:09:38 +0800

    Server - Refactor some repeated functions in project controllers

--- server/src/controllers/projectControllers.js
+++ server/src/controllers/projectControllers.js
@@ -2,9 +2,9 @@ const asynchHandler = require("express-async-handler");
 
 const Project = require("../schema/ProjectSchema");
 const User = require("../schema/UserSchema");
-const Group = require("../schema/GroupSchema");
-const Task = require("../schema/TaskSchema");
 const Chat = require("../schema/ChatSchema");
+
+const projectDetails = require("../helpers/projectDetails");
 
 /**@ GET request
  * gets all the projects
@@ -16,77 +16,9 @@ const getAllProjects = asynchHandler(async (req, res) => {
   if (!projects)
     return res.status(404).json({ message: "There are no projects found" });
 
-  const projectDetails = await Promise.all(
-    projects.map(async (project) => {
-      const ownerList = await Promise.all(
-        project.owner.map(
-          async (owner) => await User.findById(owner).select("fullName").exec()
-        )
-      );
+  const allProjects = await projectDetails(projects);
 
-      const membersList = await Promise.all(
-        project.members.map(
-          async (member) =>
-            await User.findById(member).select("fullName").exec()
-        )
-      );
-
-      const groups = await Promise.all(
-        project.groups.map(
-          async (group) => await Group.findById(group).lean().exec()
-        )
-      );
-
-      const groupDetails = await Promise.all(
-        groups.map(async (group) => {
-          const tasks = await Promise.all(
-            group.tasks.map(
-              async (task) => await Task.findById(task).lean().exec()
-            )
-          );
-
-          const taskDetails = await Promise.all(
-            tasks.map(async (task) => {
-              const assigneeList = await Promise.all(
-                task.assignee.map(
-                  async (assignee) =>
-                    await User.findById(assignee).select("fullName").exec()
-                )
-              );
-
-              const assigneeName = assigneeList.map(
-                (assignee) => assignee.fullName
-              );
-
-              return {
-                ...task,
-                assigneeName,
-              };
-            })
-          );
-
-          return { ...group, taskDetails };
-        })
-      );
-
-      const chatHistoryDetails = await Chat.findById(
-        project.chatHistory
-      ).exec();
-
-      const ownerName = ownerList.map((owner) => owner.fullName);
-      const membersName = membersList.map((member) => member.fullName);
-
-      return {
-        ...project,
-        ownerName,
-        membersName,
-        groupDetails,
-        chatHistoryDetails,
-      };
-    })
-  );
-
-  return res.status(200).json(projectDetails);
+  return res.status(200).json(allProjects);
 });
 
 /**@ GET request
@@ -100,77 +32,9 @@ const getUserProjects = asynchHandler(async (req, res) => {
     $or: [{ owner: id }, { members: id }],
   }).lean();
 
-  const projectDetails = await Promise.all(
-    projects.map(async (project) => {
-      const ownerList = await Promise.all(
-        project.owner.map(
-          async (owner) => await User.findById(owner).select("fullName").exec()
-        )
-      );
+  const userProjects = await projectDetails(projects);
 
-      const membersList = await Promise.all(
-        project.members.map(
-          async (member) =>
-            await User.findById(member).select("fullName").exec()
-        )
-      );
-
-      const groups = await Promise.all(
-        project.groups.map(
-          async (group) => await Group.findById(group).lean().exec()
-        )
-      );
-
-      const groupDetails = await Promise.all(
-        groups.map(async (group) => {
-          const tasks = await Promise.all(
-            group.tasks.map(
-              async (task) => await Task.findById(task).lean().exec()
-            )
-          );
-
-          const taskDetails = await Promise.all(
-            tasks.map(async (task) => {
-              const assigneeList = await Promise.all(
-                task.assignee.map(
-                  async (assignee) =>
-                    await User.findById(assignee).select("fullName").exec()
-                )
-              );
-
-              const assigneeName = assigneeList.map(
-                (assignee) => assignee.fullName
-              );
-
-              return {
-                ...task,
-                assigneeName,
-              };
-            })
-          );
-
-          return { ...group, taskDetails };
-        })
-      );
-
-      const chatHistoryDetails = await Chat.findById(
-        project.chatHistory
-      ).exec();
-
-      const ownerName = ownerList.map((owner) => owner.fullName);
-      const membersName = membersList.map((member) => member.fullName);
-
-      return {
-        ...project,
-        ownerName,
-        membersName,
-        groupDetails,
-        chatHistoryDetails,
-      };
-    })
-  );
-
-  return res.status(200).json(projectDetails);
+  return res.status(200).json(userProjects);
 });
 
 /**@ POST request
@@ -231,67 +95,10 @@ const updateProject = asynchHandler(async (req, res) => {
 
   const result = await foundProject.save();
 
-  const ownerList = await Promise.all(
-    result.owner.map(
-      async (owner) => await User.findById(owner).select("fullName").exec()
-    )
-  );
-
-  const membersList = await Promise.all(
-    result.members.map(
-      async (member) => await User.findById(member).select("fullName").exec()
-    )
-  );
-
-  const groups = await Promise.all(
-    result.groups.map(
-      async (group) => await Group.findById(group).lean().exec()
-    )
-  );
-
-  const groupDetails = await Promise.all(
-    groups.map(async (group) => {
-      const tasks = await Promise.all(
-        group.tasks.map(async (task) => await Task.findById(task).lean().exec())
-      );
-
-      const taskDetails = await Promise.all(
-        tasks.map(async (task) => {
-          const assigneeList = await Promise.all(
-            task.assignee.map(
-              async (assignee) =>
-                await User.findById(assignee).select("fullName").exec()
-            )
-          );
-
-          const assigneeName = assigneeList.map(
-            (assignee) => assignee.fullName
-          );
-
-          return {
-            ...task,
-            assigneeName,
-          };
-        })
-      );
-
-      return { ...group, taskDetails };
-    })
-  );
-
-  const chatHistoryDetails = await Chat.findById(result.chatHistory).exec();
-
-  const ownerName = ownerList.map((owner) => owner.fullName);
-  const membersName = membersList.map((member) => member.fullName);
+  const updatedProject = await projectDetails([foundProject]);
 
   if (result === foundProject) {
-    res.status(200).json({
-      ...result.toObject(),
-      ownerName,
-      membersName,
-      groupDetails,
-      chatHistoryDetails,
-    });
+    res.status(200).json(updatedProject);
   } else {
     res
       .status(400)
