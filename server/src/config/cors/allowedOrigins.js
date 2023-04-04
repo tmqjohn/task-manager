1ff1ed0813d421386e47ec5b349b70b0138d85ad
John Rey <tmq.john.t@gmail.com>
2023-04-05 03:23:47 +0800

    Server - Update cors policy

--- server/src/config/cors/allowedOrigins.js
+++ server/src/config/cors/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "http://167.172.109.166:5173",
+  "http://167.172.109.166",
 ];
 
 module.exports = allowedOrigins;
