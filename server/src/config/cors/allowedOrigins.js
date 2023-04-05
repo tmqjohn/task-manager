14036b1a14961fd8d63ed48b097b3c5027aa898b
John Rey <tmq.john.t@gmail.com>
2023-04-05 17:32:27 +0800

    Server - Add additional cors origins

--- server/src/config/cors/allowedOrigins.js
+++ server/src/config/cors/allowedOrigins.js
@@ -2,6 +2,9 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://167.172.109.166",
+  "https://167.172.109.166",
+  "http://167.172.109.166.nip.io",
+  "https://167.172.109.166.nip.io",
 ];
 
 module.exports = allowedOrigins;
