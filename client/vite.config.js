b50eee04e7b671bcfced7c5af17441bc663bf308
John Rey <tmq.john.t@gmail.com>
2023-04-05 17:10:05 +0800

    Client - Revise server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://167.172.109.166/",
+  base: "http://167.172.109.166/",
   server: {
     port: "80",
     host: true,
