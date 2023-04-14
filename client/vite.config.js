106f92799e61c1e6646a87935e8466390fffd29e
John Rey <tmq.john.t@gmail.com>
2023-04-15 03:56:09 +0800

    Change port settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), basicSsl()],
   base: "https://167.172.109.166.nip.io/",
   server: {
-    port: "8080",
+    port: "5000",
     host: true,
   },
 });
