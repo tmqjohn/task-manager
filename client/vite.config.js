7810f9c7ebf56d68d120dac93c88e6a42bdaaa8e
John Rey <tmq.john.t@gmail.com>
2023-04-15 03:01:50 +0800

    Client - Add basic ssl certification in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "http://167.172.109.166/",
+  plugins: [react(), basicSsl()],
+  base: "https://167.172.109.166/",
   server: {
     port: "80",
     host: true,
