2d973fabe041251fc7c925d0c98f0f89a25979ce
John Rey <tmq.john.t@gmail.com>
2023-04-15 02:52:04 +0800

    Server - Include some CORS origin

--- server/src/config/cors/allowedOrigins.js
+++ server/src/config/cors/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
+  "https://localhost:5173",
+  "https://127.0.0.1:5173",
   "http://167.172.109.166",
   "https://167.172.109.166",
   "http://167.172.109.166.nip.io",
